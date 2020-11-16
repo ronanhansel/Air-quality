c3e55f78d547a32a2de2673b95fcdf92127f9d64
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-11-16 17:46:28 +0700

    Update testsending.ino

--- air_quality_hardware/testsending/testsending.ino
+++ air_quality_hardware/testsending/testsending.ino
@@ -1,18 +1,45 @@
-int co;
-int carbon_dioxide;
-int air_quality;
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+int five;
+int seven;
+int onethreefive;
+
+
+LiquidCrystal_I2C lcd(0x27,20,4);
+
 void setup() {
   Serial.begin(9600);
+  lcd.init();
+  lcd.init();
   }
  void loop() {
-  co = analogRead(A0);
-  carbon_dioxide = analogRead(A1);
-  air_quality = analogRead(A2);
-  Serial.print(co);
-  Serial.println(" Gas");
-  Serial.print(carbon_dioxide);
-  Serial.println(" CO2");
-  Serial.print(air_quality);
-  Serial.println(" Air Quality");
+fivees();
+sevens();
+onethreefives();
+  }
+
+ void fivees() {
+  five = analogRead(A0);
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print(five);
+  lcd.setCursor(5,0);
+  lcd.print("Gas");
+  }
+ void sevens() {
+  seven = analogRead(A1);
+  lcd.backlight();
+  lcd.setCursor(0,1);
+  lcd.print(seven);
+  lcd.setCursor(5,1);
+  lcd.print("CO");
+    }
+ void onethreefives() {
+  onethreefive = analogRead(A2);
+  lcd.backlight();
+  lcd.setCursor(0,2);
+  lcd.print(onethreefive);
+  lcd.setCursor(5,2);
+  lcd.print("Air Quality");
   delay(1000);
   }
