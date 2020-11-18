9bfc82e0fd3321c4f02c03a71ea4bf71ae7d66ff
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-11-18 11:19:56 +0700

    firebase

--- air_quality_hardware/testreceiving/testreceiving.ino
+++ air_quality_hardware/testreceiving/testreceiving.ino
@@ -1,11 +1,50 @@
-int data; //Initialized variable to store recieved data
+#include <FirebaseArduino.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  //Serial Begin at 9600 Baud 
-  Serial.begin(9600);
+#define WIFI_SSID ":L"
+#define WIFI_PASSWORD "84568456"
+#define FIREBASE_HOST "https://airify-b9e29.firebaseio.com/"
+#define FIREBASE_AUTH "myh0iX4HQA1TmZZWdRUr6pfObcWGJRK71TwOixZL"
+void setup()
+{
+Serial.begin(9600);
+
+  delay(2000);
+  
+  wifiConnect();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+  delay(10);
 }
 
-void loop() {
-  data = Serial.read(); //Read the serial data and store it
-  delay(1000);
+void loop()
+{  
+if(WiFi.status() != WL_CONNECTED)
+{
+  wifiConnect();
+
+}
+delay(10);
+
+}
+
+void wifiConnect()
+{
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);             // Connect to the network
+
+  int teller = 0;
+  while (WiFi.status() != WL_CONNECTED)
+  {                                       // Wait for the Wi-Fi to connect
+    delay(1000);
+    Serial.print(++teller); Serial.print(' ');
+        // set value 
+  Firebase.setFloat("number", 42.0); 
+  // handle error 
+  if (Firebase.failed()) { 
+      Serial.print("setting /number failed:"); 
+      Serial.println(Firebase.error());   
+      return; 
+  } 
+  }
 }
