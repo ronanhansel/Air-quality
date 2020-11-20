fa4415bc308a50b8e22a06d1f68f8ce0c3ac3583
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-11-20 23:22:07 +0700

    fixed values + DHT sensor

--- air_quality_hardware/testsending/testsending.ino
+++ air_quality_hardware/testsending/testsending.ino
@@ -5,6 +5,11 @@
 #define anInput     A2                        //analog feed from MQ135
 #define co2Zero     55                        //calibrated CO2 0 level
 
+#include "DHT.h"
+#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 6
+
+DHT dht(DHTPIN, DHTTYPE);
 float five;
 float seven;
 float onethreefive;
@@ -19,7 +24,6 @@ float RO;
   int co2raw = 0;                               //int for raw value of co2
   int co2ppm = 0;                               //int for calculated ppm
   int zzz = 0;                                  //int for averaging
-SoftwareSerial s(0,1);
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup() {
@@ -27,15 +31,17 @@ void setup() {
   pinMode(anInput,INPUT); 
   lcd.init();
   lcd.init();
-  s.begin(9600);
+  lcd.backlight();
+  dht.begin();
   }
  void loop() {
-R0();
-fivees();
-sevens();
-onethreefives();
-sends();
-delay(1000);
+    R0();
+    fivees();
+    sevens();
+    onethreefives();
+    humid_temp();
+    sends();
+    delay(1000);
   }
  void sends() {
    f = String(five)+String(" ")+String(seven)+String(" ")+String(onethreefive)+String(" ");
@@ -43,30 +49,39 @@ delay(1000);
   }
  void fivees() {
   five = analogRead(A0);
-  lcd.backlight();
   lcd.setCursor(0,0);
   lcd.print(five);
-  lcd.setCursor(5,0);
+  lcd.setCursor(16,0);
   lcd.print("Gas");
   }
  void sevens() {
   seven = seven_ppm();
-  lcd.backlight();
   lcd.setCursor(0,1);
   lcd.print(seven);
-  lcd.setCursor(5,1);
+  lcd.setCursor(16,1);
   lcd.print("CO");
     }
  void onethreefives() {
-  onethreefive = co2();
-  lcd.backlight();
+  onethreefive = analogRead(A2);
   lcd.setCursor(0,2);
   lcd.print(onethreefive);
-  lcd.setCursor(5,2);
-  lcd.print("Air Quality");
+  lcd.setCursor(16,2);
+  lcd.print("CO2");
+  }
+ void humid_temp() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  lcd.setCursor(0,3);
+  lcd.print(h);
+  lcd.setCursor(6,3);
+  lcd.print("Humid");
+  lcd.setCursor(12,3);
+  lcd.print(t);
+  lcd.setCursor(18,3);
+  lcd.print("C");
   }
 void R0() {
-  int sensorValue = analogRead(A0);
+  int sensorValue = analogRead(A1);
   sensor_volt=(float)sensorValue/1024*5.0;
   RS_gas = ((5.0 * R2)/sensor_volt) - R2;
   RO = RS_gas / 1;
@@ -79,37 +94,4 @@ float seven_ppm() {
    float x = 1538.46 * ratio;
    float ppm = pow(x,-1.709);
    return (ppm);
-}
-
-
-float onethreefive_ppm() {
-   sensorValue = analogRead(A2);
-   sensor_volt = (float)sensorValue/1024*5.0;
-   RS_gas = (5.0-sensor_volt)/sensor_volt;
-   ratio = RS_gas/RO; //Replace RO with the value found using the sketch above
-   float x = 1538.46 * ratio;
-   float ppm = pow(x,-1.709);
-   return (ppm);
-}
-  
-int co2() 
-{
-  
-
-
-
-  for (int x = 0;x<10;x++)  //samplpe co2 10x over 2 seconds
-  {                   
-    co2now[x]=analogRead(A2);
-    delay(200);
-  }
-
-  for (int x = 0;x<10;x++)  //add samples together
-  {                     
-    zzz=zzz + co2now[x];  
-  }
-  co2raw = zzz/10;                            //divide samples by 10
-  co2ppm = co2raw - co2Zero;                 //get calculated ppm
-  return abs(co2ppm);            
-
 }
