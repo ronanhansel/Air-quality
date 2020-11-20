625dd04f258d4a6351e733ddddb3fdc27f527da9
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2020-11-20 10:28:49 +0700

    Finished hardware

--- air_quality_hardware/testreceiving/testreceiving.ino
+++ air_quality_hardware/testreceiving/testreceiving.ino
@@ -1,50 +1,67 @@
+#include <Firebase.h>
 #include <FirebaseArduino.h>
+#include <FirebaseCloudMessaging.h>
+#include <FirebaseError.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseObject.h>
+#include <SoftwareSerial.h>
+
+
 #include <ESP8266WiFi.h>
 
 #define WIFI_SSID ":L"
 #define WIFI_PASSWORD "84568456"
-#define FIREBASE_HOST "https://airify-b9e29.firebaseio.com/"
+#define FIREBASE_HOST "airify-b9e29.firebaseio.com"
 #define FIREBASE_AUTH "myh0iX4HQA1TmZZWdRUr6pfObcWGJRK71TwOixZL"
+int five;
+int seven;
+int onethreefive;
+int placeone;
+int placetwo;
+int placethree;
+String number;
+
+SoftwareSerial s(3,1);
 void setup()
 {
-Serial.begin(9600);
 
-  delay(2000);
-  
-  wifiConnect();
-
+  Serial.begin(9600);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.print(". ");
+    delay(1000);
+    } 
+    Serial.println("Connected");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-
-  delay(10);
 }
 
 void loop()
 {  
-if(WiFi.status() != WL_CONNECTED)
-{
-  wifiConnect();
+  if (Serial.available()) {
+  number = Serial.readString();
+  int n = number.length();
+  char char_array[n + 1];
+  strcpy(char_array, number.c_str());
+  char *split = strtok(char_array, " ");
+  char *splittwo = strtok(NULL, " ");
+  char *splitthree = strtok(NULL, " ");
+  five = atoi(split);
+  seven = atoi(splittwo);
+  onethreefive = atoi(splitthree);
 
-}
-delay(10);
 
-}
+  Firebase.setInt("5", five);
+  Firebase.setInt("7", seven); 
+  Firebase.setInt("135", onethreefive);
+  
 
-void wifiConnect()
-{
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);             // Connect to the network
 
-  int teller = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {                                       // Wait for the Wi-Fi to connect
-    delay(1000);
-    Serial.print(++teller); Serial.print(' ');
-        // set value 
-  Firebase.setFloat("number", 42.0); 
   // handle error 
   if (Firebase.failed()) { 
-      Serial.print("setting /number failed:"); 
-      Serial.println(Firebase.error());   
+      Serial.println("setting number failed:"); 
+      Serial.println(Firebase.error());  
       return; 
+    }
   } 
-  }
+    delay(1000);
 }
--- air_quality_hardware/testsending/testsending.ino
+++ air_quality_hardware/testsending/testsending.ino
@@ -1,23 +1,46 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
-int five;
-int seven;
-int onethreefive;
 
+#include<SoftwareSerial.h>
+#define anInput     A2                        //analog feed from MQ135
+#define co2Zero     55                        //calibrated CO2 0 level
 
+float five;
+float seven;
+float onethreefive;
+  String f;
+int R2 = 2000;
+int sensorValue;
+float sensor_volt;
+float RS_gas;
+float ratio;
+float RO;
+  int co2now[10];                               //int array for co2 readings
+  int co2raw = 0;                               //int for raw value of co2
+  int co2ppm = 0;                               //int for calculated ppm
+  int zzz = 0;                                  //int for averaging
+SoftwareSerial s(0,1);
 LiquidCrystal_I2C lcd(0x27,20,4);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(anInput,INPUT); 
   lcd.init();
   lcd.init();
+  s.begin(9600);
   }
  void loop() {
+R0();
 fivees();
 sevens();
 onethreefives();
+sends();
+delay(1000);
   }
-
+ void sends() {
+   f = String(five)+String(" ")+String(seven)+String(" ")+String(onethreefive)+String(" ");
+  Serial.print(f);
+  }
  void fivees() {
   five = analogRead(A0);
   lcd.backlight();
@@ -27,7 +50,7 @@ onethreefives();
   lcd.print("Gas");
   }
  void sevens() {
-  seven = analogRead(A1);
+  seven = seven_ppm();
   lcd.backlight();
   lcd.setCursor(0,1);
   lcd.print(seven);
@@ -35,11 +58,58 @@ onethreefives();
   lcd.print("CO");
     }
  void onethreefives() {
-  onethreefive = analogRead(A2);
+  onethreefive = co2();
   lcd.backlight();
   lcd.setCursor(0,2);
   lcd.print(onethreefive);
   lcd.setCursor(5,2);
   lcd.print("Air Quality");
-  delay(1000);
   }
+void R0() {
+  int sensorValue = analogRead(A0);
+  sensor_volt=(float)sensorValue/1024*5.0;
+  RS_gas = ((5.0 * R2)/sensor_volt) - R2;
+  RO = RS_gas / 1;
+}
+float seven_ppm() {
+   sensorValue = analogRead(A1);
+   sensor_volt = (float)sensorValue/1024*5.0;
+   RS_gas = (5.0-sensor_volt)/sensor_volt;
+   ratio = RS_gas/RO; //Replace RO with the value found using the sketch above
+   float x = 1538.46 * ratio;
+   float ppm = pow(x,-1.709);
+   return (ppm);
+}
+
+
+float onethreefive_ppm() {
+   sensorValue = analogRead(A2);
+   sensor_volt = (float)sensorValue/1024*5.0;
+   RS_gas = (5.0-sensor_volt)/sensor_volt;
+   ratio = RS_gas/RO; //Replace RO with the value found using the sketch above
+   float x = 1538.46 * ratio;
+   float ppm = pow(x,-1.709);
+   return (ppm);
+}
+  
+int co2() 
+{
+  
+
+
+
+  for (int x = 0;x<10;x++)  //samplpe co2 10x over 2 seconds
+  {                   
+    co2now[x]=analogRead(A2);
+    delay(200);
+  }
+
+  for (int x = 0;x<10;x++)  //add samples together
+  {                     
+    zzz=zzz + co2now[x];  
+  }
+  co2raw = zzz/10;                            //divide samples by 10
+  co2ppm = co2raw - co2Zero;                 //get calculated ppm
+  return abs(co2ppm);            
+
+}
