a3ab1c117acbee6fd14c6a964d2b18e75af61bf8
Trần Vũ Mạnh Đức <64572619+ronanhansel@users.noreply.github.com>
2021-07-25 23:22:40 +0700

    finalisation

--- air_quality_app/lib/main.dart
+++ air_quality_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:air_quality/pages/air_quality.dart';
 import 'package:air_quality/pages/co2.dart';
 import 'package:air_quality/pages/covid.dart';
-import 'package:air_quality/pages/graph.dart.bak';
 import 'package:air_quality/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
